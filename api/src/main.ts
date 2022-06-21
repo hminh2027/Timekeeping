69917fbfc0f9cf99613374f0b6a0e5546d23722a
Dev Hau <50167033+devhau@users.noreply.github.com>
2022-06-21 15:07:24 +0700

    add code backend

---
+++ api/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
+  const config = new DocumentBuilder()
+    .setTitle('api vcheck document')
+    .setDescription('api vcheck document')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(3000);
+  console.log('localhost:3000')
+}
+bootstrap();
