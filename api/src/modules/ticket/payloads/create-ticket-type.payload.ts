10f90083eb49ffd8e8aa64cc523eee4c5c0f2092
minh2027 <minh.vuhoang@vdtsol.com>
2022-07-05 10:34:18 +0700

    replace ticket type and ticket status entities by enums

--- api/src/modules/ticket/payloads/create-ticket-type.payload.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
-
-export class CreateTicketTypePayload {
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    name: string;
-}
