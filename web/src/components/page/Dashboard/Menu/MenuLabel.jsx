57b987e8aa3efc834ae8b4a89dbafe27345f0f5a
pta208VDT <anh.phan@vdtsol.com>
2022-07-19 09:22:38 +0700

    switching to tailwindcss

--- web/src/components/page/Dashboard/Menu/MenuLabel.jsx
+++ web/src/components/page/Dashboard/Menu/MenuLabel.jsx
@@ -17,11 +17,11 @@ const MenuLabel = (props) => {
   if (curItem === props.id) {
     color.push("rgb(205, 240, 234)");
   }
-  console.log(props.id)
+  console.log(props.id);
   return (
     <Link href={href}>
       <div
-        className="flex items-center py-4 flex-col flex-grow cursor-pointer lg:flex-grow-0 lg:flex-row  lg:gap-4 lg:text-lg lg:p-4 hover:bg-gray-100 active:bg-primary"
+        className="flex items-center justify-center p-4 flex-col flex-grow cursor-pointer lg:flex-grow-0 lg:flex-row  lg:gap-4 lg:text-lg lg:p-4 lg:justify-start hover:bg-gray-100 active:bg-primary"
         style={{ backgroundColor: color[0] }}
         onClick={() => {
           dispatch(changeCurrentItem({ menuItem: id }));
