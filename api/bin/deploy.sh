f6aae6eb939a2c2d94eab3b0083960c942dae8bb
nguyen-vdt <nguyen.pham@vdtsol.com>
2022-07-20 14:31:19 +0700

    update script

--- api/bin/deploy.sh
+++ api/bin/deploy.sh
@@ -3,4 +3,5 @@ git stash --include-untracked
 git reset --hard deploy/api
 git pull https://$GIT_TOKEN@github.com/vdtsol/vCheck deploy/api --allow-unrelated-histories
 cp .env.temp .env.development
+npm i
 pm2 reload apivcheck
