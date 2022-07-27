a6ffdfae46c16fe957119ae27253470b4af5db0c
minh2027 <minh.vuhoang@vdtsol.com>
2022-07-27 08:55:47 +0700

    fix bug

--- web/src/components/page/Dashboard/Home/ReactCalendar.jsx
+++ web/src/components/page/Dashboard/Home/ReactCalendar.jsx
@@ -115,7 +115,7 @@ const ReactCalendar = () => {
             src={`${process.env.APP_URL}${
               checkInInfo && checkInInfo.checkInImage
             }`}
-            className="w-full h-full aspect-video object-contain"
+            className="object-contain w-full h-full aspect-video"
             crossOrigin="anonymous"
           />
         </div>
@@ -134,7 +134,7 @@ const ReactCalendar = () => {
           {checkInInfo && checkInInfo.checkOutImage && (
             <img
               src={`${process.env.APP_URL}${checkInInfo.checkOutImage}`}
-              className="w-full h-full aspect-video object-contain"
+              className="object-contain w-full h-full aspect-video"
               crossOrigin="anonymous"
             />
           )}
