63534e45487a05d10765b3cdbf839a402dcadd38
pta208VDT <anh.phan@vdtsol.com>
2022-07-28 16:18:35 +0700

    add Blur

--- web/src/components/page/Dashboard/Home/index.jsx
+++ web/src/components/page/Dashboard/Home/index.jsx
@@ -22,37 +22,39 @@ const Home = () => {
     </>
   );
   const checkInContent = (
-    <>
+    <div className="flex flex-col justify-center gap-8">
       <div>Already Checked In!🔥🔥🔥</div>
       {checkInInfo && (
-        <img
-          crossOrigin="anonymous"
-          src={`${process.env.APP_URL}${checkInInfo.checkinImage}`}
-          // width="16"
-          // height="9"
-          // layout="responsive"
-          className="object-contain aspect-video"
-        />
+        <div className="flex items-center justify-center blur-xl hover:blur-0">
+          <img
+            crossOrigin="anonymous"
+            src={`${process.env.APP_URL}${checkInInfo.checkinImage}`}
+            // width="16"
+            // height="9"
+            // layout="responsive"
+            className="aspect-video w-full rounded-lg object-contain "
+          />
+        </div>
       )}
-    </>
+    </div>
   );
   return (
     <>
-      <div className="flex gap-8 m-4">
-        <div className="flex flex-col w-1/3 gap-4">
+      <div className="m-4 flex gap-8">
+        <div className="flex w-1/3 flex-col gap-4">
           <div className="card ">
             <div className="card-body">
               {checkInStatus ? checkInContent : notCheckedContent}
             </div>
           </div>
-          <div className="w-full card">
+          <div className="card w-full">
             <div className="card-body">
               <ReactCalendar />
             </div>
           </div>
         </div>
-        <div className="flex flex-col w-2/3 gap-4">
-          <div className="w-full card">
+        <div className="flex w-2/3 flex-col gap-4">
+          <div className="card w-full">
             <div className="card-body">
               <LeaderBoard />
             </div>
