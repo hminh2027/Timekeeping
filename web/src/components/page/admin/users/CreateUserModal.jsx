669e725951ed379a39cdba7936c4bdb7d8d748d1
mlinh <mylinh.hoang@vdtsol.com>
2022-07-28 16:38:54 +0700

    fix create button ui

--- web/src/components/page/admin/users/CreateUserModal.jsx
+++ web/src/components/page/admin/users/CreateUserModal.jsx
@@ -23,100 +23,87 @@ const CreateUser = (props) => {
   }
   const handleChange = (e) => {
     setUserData({ ...UserData, [e.target.name]: e.target.value });
+    console.log(UserData)
   };
   return (
-      <div className="card">
-        <div className="card-body w-full"> 
-          <div className=" justify-center text-center text-xl font-bold  text-emerald-600">
+    <div className="card w-[500px] lg:w-[600px] xl:w-[700px]  ">
+      <div className="card-body flex flex-col">
+        <div
+          className="bg-yellow-400 w-1/3"
+        >
+        </div>
+        <div>
+          <div className="flex-1 justify-center text-center text-xl font-bold  text-emerald-500">
             Create User
           </div>
-          <div className="border-[1px] border-double border-green-600 mb-6 mt-[-1px]"></div>
+          <div className="mb-6 mt-[-1px] border-[1px] border-double border-emerald-400"></div>
           <div className={styles[`input-wrapper`]}>
             <div className={styles[`input-list`]}>
-              {/* <div className="flex ">
-                <div className="w-4/12 justify-center p-2 text-center text-sm font-medium dark:text-gray-300">
-                  Email:
-                </div>
-                <input
-                  className="flex-1 border border-solid border-gray-300 p-2 text-gray-500"
-                  type="text"
-                  placeholder="Email"
-                  name="email"
-                  value={UserData.email}
-                  onChange={(e) => {
-                    handleChange(e);
-                  }}
-                />
-              </div> */}
-              <div className="flex items-center gap-4">
-                <div style={{ minWidth: "5em" }}>Email:</div>
-                <input
-                  type="text"
-                  name="email"
-                  placeholder="Email"
-                  value={UserData.email}
-                  className="v-input flex-1"
-                  onChange={(e) => {
-                    handleChange(e);
-                  }}
-                />
-              </div>
-              <div className="flex ">
-                <div className="w-4/12 justify-center p-2 text-center text-sm font-medium dark:text-gray-300">
-                  FirstName:
-                </div>
-                <input
-                  className="flex-1 border border-solid border-gray-300 p-2 text-gray-500"
-                  type="text"
-                  name="firstName"
-                  placeholder="FirstName"
-                  value={UserData.firstName}
-                  onChange={(e) => {
-                    handleChange(e);
-                  }}
-                />
-              </div>
-              <div className="flex ">
-                <div className="w-4/12 justify-center p-2 text-center text-sm font-medium dark:text-gray-300">
-                  LastName:
-                </div>
-                <input
-                  className="flex-1 border border-solid border-gray-300 p-2 text-gray-500"
-                  type="text"
-                  name="lastName"
-                  placeholder="LastName"
-                  value={UserData.lastName}
-                  onChange={(e) => {
-                    handleChange(e);
-                  }}
-                />
-              </div>
-              <div className="flex">
-                <div className="w-4/12 justify-center p-2 text-center text-sm font-medium dark:text-gray-300">
-                  Password:
-                </div>
-                <input
-                  type="password"
-                  className=" flex-1 border border-solid border-gray-300 p-2 text-gray-500"
-                  name="password"
-                  value={UserData.password}
-                  placeholder="Password"
-                  onChange={(e) => {
-                    handleChange(e);
-                  }}
-                ></input>
-              </div>
+              <Input
+                type="text"
+                name={"Email"}
+                nameInput={"email"}
+                hanlde={(e) => handleChange(e)}
+                value= {UserData.email}
+              />
+              <Input
+                type="text"
+                name={"FirstName"}
+                nameInput={"firstName"}
+                hanlde={(e)=> handleChange(e)}
+                value={UserData.firstName}
+              />
+              <Input
+                type="text"
+                name={"LastName"}
+                nameInput={"lastName"}
+                hanlde={(e)=> handleChange(e)}
+                value={UserData.lastName}
+              />
+              <Input
+                type="password"
+                name={"Password"}
+                nameInput={"password"}
+                hanlde={(e)=> handleChange(e)}
+                value={UserData.password}
+              />
             </div>
           </div>
-          <button
-            onClick={() => handleSubmit(UserData)}
-            className="m-auto mt-3 w-1/3 rounded-lg border border-solid border-teal-600 p-1 text-black shadow-xl hover:bg-teal-600 hover:text-white"
-          >
-            Submit
-          </button>
+          <div className="flex justify-end">
+            <button
+              onClick={() => handleSubmit(UserData)}
+              className="self-end mt-3 w-1/3 rounded-lg border border-solid border-teal-600 p-1 text-black hover:shadow-xl hover:bg-teal-600 hover:text-white"
+            >
+              Submit
+            </button>
+            <button
+              onClick={() => handleSubmit(UserData)}
+              className="self-end ml-3 mt-3 w-1/3 rounded-lg border border-solid border-gray-500 p-1 text-black hover:shadow-xl hover:bg-gray-500 hover:text-white"
+            >
+              Close
+            </button>
+          </div>
+          
         </div>
       </div>
+    </div>
   );
 };
+
+const Input = (props) => {
+  return (
+    <div className="flex items-center gap-4">
+      <div style={{ minWidth: "5em" }}>{props.name}:</div>
+      <input
+        type={props.type}
+        name={props.nameInput}
+        placeholder={props.name}
+        value={props.value}
+        className="v-input flex-1"
+        onChange={props.hanlde}
+      />
+    </div>
+  )
+}
 
 export default CreateUser;
