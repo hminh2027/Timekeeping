104bdc83f02ac11af9be87cb2d85eba62905aeab
minh2027 <minh.vuhoang@vdtsol.com>
2022-08-04 21:13:06 +0700

    migration file

--- api/src/migrations/1659621542901-init.ts
+++ api/src/migrations/1659621542901-init.ts
@@ -2,6 +2,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class init1659621542901 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('drop table `contacts`');
     await queryRunner.query(
       'alter table `users` add column address varchar(255) NULL default "" ',
     );
