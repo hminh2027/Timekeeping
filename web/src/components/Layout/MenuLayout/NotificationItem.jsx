27c8be148b36b0f7cc2f39ffed38aedfb55968bc
mlinh <mylinh.hoang@vdtsol.com>
2022-08-05 10:20:22 +0700

    fix notification, chart

--- web/src/components/Layout/MenuLayout/NotificationItem.jsx
+++ web/src/components/Layout/MenuLayout/NotificationItem.jsx
@@ -1,7 +1,21 @@
+import moment from 'moment';
 const NotificationItem = ({notification,...props}) => {
+    const {author} = notification
     return (
-        <div className={`p-3 hover:cursor-pointer rounded-lg flex flex-row items-center hover:bg-slate-500 ${notification.isRead ? "text-slate-600" : "bg-slate-300 text-black"}` }>
-           <p className="text-lg text-center font-extrabold text-black">{notification.content}</p> 
+        <div className={`p-3 hover:cursor-pointer rounded-lg flex flex-row items-center hover:bg-slate-300` }>
+            <div className="flex">
+                <img src={author.avatar} alt="avatar" className="w-[56px] h-[56px] rounded-full mr-1"/>
+                <div className="flex-1">
+                    <p className={`w-full text-base text-left text-black ${notification.isRead? "font-normal":"font-extrabold"}`}>
+                        {notification.content.replace(notification.authorId,author.lastName+" "+author.firstName)}
+                    </p> 
+                    <p className="text-left text-sm text-slate-500">
+                        {moment(notification.createdAt).fromNow()}
+                    </p>
+                </div>
+
+            </div>
+
         </div>
     )
 }
